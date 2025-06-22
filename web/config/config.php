aaf7cff0c446e52ce65d381f1a644644cee4c95e
Astraeakle <ghuertashuallcca@gmail.com>
2025-06-21 20:17:40 -0500

    subir el config para no perderlo?

---
+++ web/config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'simpro_lite'); // Cambia por el nombre de tu base de datos
+define('DB_USER', 'root'); // Usuario de MySQL (por defecto en XAMPP)
+define('DB_PASSWORD', '0000'); // Contraseña (vacía por defecto en XAMPP)
+date_default_timezone_set('America/Lima'); // Ajusta según tu zona horaria
+
+?>
